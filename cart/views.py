53481cbe33404ce85bd8cdae2de70bbcd7dd1b38
rajaonahsoa <r.lalainafr@gmail.com>
2024-10-14 20:22:20 +0200

    create cart page

---
+++ cart/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def cart_summary(request):
+    context = {}
+    return render(request, 'cart/cart-summary.html', context)
+
+def cart_add(request):
+    pass
+
+def cart_delete(request):
+    pass
+
+def cart_update(request):
+    pass
--- og_project/settings.py
+++ og_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ticket'
+    'ticket',
+    'cart'
 ]
 
 MIDDLEWARE = [
