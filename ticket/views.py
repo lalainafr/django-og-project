24d6a613a2485df46fd558699d96f0a83edf1e1c
rajaonahsoa <r.lalainafr@gmail.com>
2024-10-14 19:55:37 +0200

    split template part: nav-foot-header-content

--- ticket/views.py
+++ ticket/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from ticket.models import Product
 
 def home(request):
-    return render(request, 'ticket/index.html', {})
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'ticket/index.html', context )
